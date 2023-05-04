2f03d68c65b32517a3cacd34d8ad9bcb85841e4b
malyavin.dd <malyavin.dd@outlook.com>
2023-05-04 19:30:27 +0300

    Checking if the import error has been resolved

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"fmt"
-	"github.com/MlDenis/dm-go-musthave-metrics/internal/handlers"
+	"github.com/MlDenis/dm-go-musthave-metrics/internal/metric"
 	"github.com/MlDenis/dm-go-musthave-metrics/internal/storage"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type MSServer struct {
@@ -19,8 +21,53 @@ func MakeNewMSServer(adress string) MSServer {
 
 func (s *MSServer) DoTheJob() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", handlers.HandlerPostMetricDataAsText)
-	mux.HandleFunc("/update/", handlers.HandlerPostMetricDataAsText)
+	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+
+		segmentsData := strings.Split(r.URL.Path, "/")
+		if len(segmentsData) != 5 {
+			http.Error(w, "Incorrect request", http.StatusBadRequest)
+			return
+		}
+		vt := segmentsData[2]
+		name := segmentsData[3]
+		value := segmentsData[4]
+
+		switch vt {
+		case metric.GaugeString:
+			convertedValueFloat, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				http.Error(w, "Incorrect data", http.StatusBadRequest)
+				return
+			}
+			convertedValueGauge := metric.Gauge(convertedValueFloat)
+
+			s.MS.UpdateMetricInStorage(
+				vt,
+				name,
+				convertedValueGauge,
+				-1)
+			w.WriteHeader(http.StatusOK)
+			return
+		case metric.CounterString:
+			convertedValueInt, err := strconv.ParseInt(value, 0, 64)
+			if err != nil {
+				http.Error(w, "Incorrect data", http.StatusBadRequest)
+				return
+			}
+			convertedValueCounter := metric.Counter(convertedValueInt)
+
+			s.MS.UpdateMetricInStorage(
+				vt,
+				name,
+				0.0,
+				convertedValueCounter)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		http.Error(w, "Incorrect request", http.StatusBadRequest)
+		return
+	})
 
 	err := http.ListenAndServe(s.addr, mux)
 	if err != nil {
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"github.com/MlDenis/dm-go-musthave-metrics/internal/metric"
+	"log"
+)
+
+type MetricData struct {
+	metricType   string
+	gaugeValue   metric.Gauge
+	counterValue metric.Counter
+}
+
+type MemStorage struct {
+	data map[string]*MetricData
+}
+
+func (ms *MemStorage) NewMetricsStorage() MemStorage {
+	return MemStorage{}
+}
+
+func (ms *MemStorage) UpdateMetricInStorage(
+	metricType string,
+	metricName string,
+	gaugeValue metric.Gauge,
+	counterValue metric.Counter) {
+	switch metricType {
+	case metric.GaugeString:
+		(ms.data[metricName]).gaugeValue = gaugeValue
+		log.Printf("#DEBUG: gaugeValue %s : %v has been added to the MemStorage\n", metricName, gaugeValue)
+	case metric.CounterString:
+		ms.data[metricName].counterValue = counterValue
+		log.Printf("#DEBUG: counterValue %s : %v has been added to the MemStorage\n", metricName, counterValue)
+	}
+}
