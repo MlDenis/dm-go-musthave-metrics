d9918c1ecbff50ab93ec22650ff14ae5c1f69a98
malyavin.dd <malyavin.dd@outlook.com>
2023-05-16 03:04:36 +0300

    Added explicit names for the fields of the MetricData structure when collecting metrics.

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -22,49 +22,49 @@ func (mdf *MetricsDataBuffer) CollectMetricData() {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 
-	mdf.Data["Alloc"] = &storage.MetricData{"gauge", metric.Gauge(memStats.Alloc), -1}
-	mdf.Data["BuckHashSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.BuckHashSys), -1}
-	mdf.Data["Frees"] = &storage.MetricData{"gauge", metric.Gauge(memStats.Frees), -1}
+	mdf.Data["Alloc"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.Alloc), CounterValue: -1}
+	mdf.Data["BuckHashSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.BuckHashSys), CounterValue: -1}
+	mdf.Data["Frees"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.Frees), CounterValue: -1}
 
-	mdf.Data["GCCPUFraction"] = &storage.MetricData{"gauge", metric.Gauge(memStats.GCCPUFraction), -1}
-	mdf.Data["GCSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.GCSys), -1}
-	mdf.Data["HeapAlloc"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapAlloc), -1}
+	mdf.Data["GCCPUFraction"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.GCCPUFraction), CounterValue: -1}
+	mdf.Data["GCSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.GCSys), CounterValue: -1}
+	mdf.Data["HeapAlloc"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapAlloc), CounterValue: -1}
 
-	mdf.Data["HeapIdle"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapIdle), -1}
-	mdf.Data["HeapInuse"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapInuse), -1}
-	mdf.Data["HeapObjects"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapObjects), -1}
+	mdf.Data["HeapIdle"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapIdle), CounterValue: -1}
+	mdf.Data["HeapInuse"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapInuse), CounterValue: -1}
+	mdf.Data["HeapObjects"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapObjects), CounterValue: -1}
 
-	mdf.Data["HeapReleased"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapReleased), -1}
-	mdf.Data["HeapSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.HeapSys), -1}
-	mdf.Data["LastGC"] = &storage.MetricData{"gauge", metric.Gauge(memStats.LastGC), -1}
+	mdf.Data["HeapReleased"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapReleased), CounterValue: -1}
+	mdf.Data["HeapSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.HeapSys), CounterValue: -1}
+	mdf.Data["LastGC"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.LastGC), CounterValue: -1}
 
-	mdf.Data["Lookups"] = &storage.MetricData{"gauge", metric.Gauge(memStats.Lookups), -1}
-	mdf.Data["MCacheInuse"] = &storage.MetricData{"gauge", metric.Gauge(memStats.MCacheInuse), -1}
-	mdf.Data["MCacheSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.MCacheSys), -1}
+	mdf.Data["Lookups"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.Lookups), CounterValue: -1}
+	mdf.Data["MCacheInuse"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.MCacheInuse), CounterValue: -1}
+	mdf.Data["MCacheSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.MCacheSys), CounterValue: -1}
 
-	mdf.Data["MSpanInuse"] = &storage.MetricData{"gauge", metric.Gauge(memStats.MSpanInuse), -1}
-	mdf.Data["MSpanSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.MSpanSys), -1}
-	mdf.Data["Mallocs"] = &storage.MetricData{"gauge", metric.Gauge(memStats.Mallocs), -1}
+	mdf.Data["MSpanInuse"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.MSpanInuse), CounterValue: -1}
+	mdf.Data["MSpanSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.MSpanSys), CounterValue: -1}
+	mdf.Data["Mallocs"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.Mallocs), CounterValue: -1}
 
-	mdf.Data["NextGC"] = &storage.MetricData{"gauge", metric.Gauge(memStats.NextGC), -1}
-	mdf.Data["NumForcedGC"] = &storage.MetricData{"gauge", metric.Gauge(memStats.NumForcedGC), -1}
-	mdf.Data["NumGC"] = &storage.MetricData{"gauge", metric.Gauge(memStats.NumGC), -1}
+	mdf.Data["NextGC"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.NextGC), CounterValue: -1}
+	mdf.Data["NumForcedGC"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.NumForcedGC), CounterValue: -1}
+	mdf.Data["NumGC"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.NumGC), CounterValue: -1}
 
-	mdf.Data["OtherSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.OtherSys), -1}
-	mdf.Data["PauseTotalNs"] = &storage.MetricData{"gauge", metric.Gauge(memStats.PauseTotalNs), -1}
-	mdf.Data["StackInuse"] = &storage.MetricData{"gauge", metric.Gauge(memStats.StackInuse), -1}
+	mdf.Data["OtherSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.OtherSys), CounterValue: -1}
+	mdf.Data["PauseTotalNs"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.PauseTotalNs), CounterValue: -1}
+	mdf.Data["StackInuse"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.StackInuse), CounterValue: -1}
 
-	mdf.Data["StackSys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.StackSys), -1}
-	mdf.Data["Sys"] = &storage.MetricData{"gauge", metric.Gauge(memStats.Sys), -1}
-	mdf.Data["TotalAlloc"] = &storage.MetricData{"gauge", metric.Gauge(memStats.TotalAlloc), -1}
+	mdf.Data["StackSys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.StackSys), CounterValue: -1}
+	mdf.Data["Sys"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.Sys), CounterValue: -1}
+	mdf.Data["TotalAlloc"] = &storage.MetricData{MetricType: "gauge", GaugeValue: metric.Gauge(memStats.TotalAlloc), CounterValue: -1}
 
 	rv := metric.Gauge(rand.Float64())
-	mdf.Data["RandomValue"] = &storage.MetricData{"gauge", rv, -1}
+	mdf.Data["RandomValue"] = &storage.MetricData{MetricType: "gauge", GaugeValue: rv, CounterValue: -1}
 
 	_, ok := mdf.Data["PollCount"]
 	if ok {
 		(mdf.Data["PollCount"]).CounterValue += 1
 	} else {
-		mdf.Data["PollCount"] = &storage.MetricData{"counter", -1.0, 1}
+		mdf.Data["PollCount"] = &storage.MetricData{MetricType: "counter", GaugeValue: -1.0, CounterValue: 1}
 	}
 }
